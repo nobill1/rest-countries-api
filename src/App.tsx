a9044c489999a58f17ff4fb76b9e3f26ae580ca6
Nguimeya Bill-gates <bnguimeya007@gmail.com>
2022-02-27 04:09:30 +0100

    adding tailwindcss, themeswitcher, react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function trigger() {    
+    const body = document.body;
+    const classBody = body.classList;
+    const result = classBody.toggle('dark')
+
+    if (result) {
+      console.log('dark added')
+    } else {
+      console.log('removed dark')
+    }
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header dark:bg-fuchsia-600">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="font-bold text-4xl underline text-red-500"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <button className='py-1 px-4 bg-slate-500 mt-2 rounded-md' onClick={() => trigger()}>Change theme</button>
       </header>
     </div>
   );
